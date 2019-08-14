f3eedc0a78451bf9155f99fc1f74d478f016280f
Risheng Tan <rtan2@montefiore.org>
2019-08-14 15:36:50 -0400

    Long HTML

--- src/app/home/golden-record/golden-record.component.ts
+++ src/app/home/golden-record/golden-record.component.ts
@@ -3,8 +3,9 @@ import { MatSidenav } from '@angular/material';
 import { FlatTreeControl } from '@angular/cdk/tree';
 import { MatTreeFlatDataSource, MatTreeFlattener } from '@angular/material/tree';
 import { FormControl, Validators, FormsModule } from '@angular/forms';
-import { Route, Router } from '@angular/router';
+import { Route, Router,ActivatedRoute } from '@angular/router';
 import { GetDataService } from 'src/app/@core/service/get-data.service';
+import { ProviderData } from 'src/app/@core/mock/provider-data';
 
 interface FoodNode {
   name: string;
@@ -62,49 +63,90 @@ interface ExampleFlatNode {
   styleUrls: ['./golden-record.component.sass']
 })
 export class GoldenRecordComponent implements OnInit {
-  Source = new FormControl('', Validators.required);
-  LastName = new FormControl('Lu', [Validators.required, Validators.pattern('[a-zA-Z ]*')]);
-  MiddleName = new FormControl('Null', Validators.required);
-  FirstName = new FormControl('Kay', Validators.required);
-  NPI = new FormControl('779693442', Validators.required);
-  DOB = new FormControl('07/07/1996', Validators.required);
-  BillingAddress = new FormControl('123 Boston', Validators.required);
-  Specialty = new FormControl('Brain Surgeon', Validators.required);
-  ZipCode = new FormControl('11212', [Validators.required, Validators.pattern('[0-9]*')]);
+  BusinessKey = new FormControl('',/* [Validators.required]*/);
+  provid = new FormControl('',/* [Validators.required]*/);
+  EchoDrid = new FormControl('',/* [Validators.required]*/);
+  FirstName = new FormControl('', [Validators.required]);
+  LastName = new FormControl('', [Validators.required]);
+  MiddleName = new FormControl('',/* [Validators.required]*/);
+  ProviderFullName = new FormControl('',/* [Validators.required]*/);
+  Provtype = new FormControl('',/* [Validators.required]*/);
+  ProviderTypeDesc = new FormControl('',/* [Validators.required]*/);
+  Claimtype = new FormControl('',/* [Validators.required]*/);
+  Obs_claimtype = new FormControl('',/* [Validators.required]*/);
+  LicenseNumber = new FormControl('',/* [Validators.required]*/);
+  DOB = new FormControl('',/* [Validators.required]*/);
+  SSN = new FormControl('',/* [Validators.required]*/);
+  NPI = new FormControl('', [Validators.required]);
+  EmploymentCode = new FormControl('',/* [Validators.required]*/);
+  Specialtycode = new FormControl('',/* [Validators.required]*/);
+  SpecialtyDescription = new FormControl('',/* [Validators.required]*/);
+  Profdesig = new FormControl('',/* [Validators.required]*/);
+  PracAddressType = new FormControl('',/* [Validators.required]*/);
+  PracAddressTypeDesc = new FormControl('',/* [Validators.required]*/);
+  Addr = new FormControl('',/* [Validators.required]*/);
+  Addr2 = new FormControl('',/* [Validators.required]*/);
+  City = new FormControl('',/* [Validators.required]*/);
+  State = new FormControl('',/* [Validators.required]*/);
+  Zip = new FormControl('',/* [Validators.required]*/);
+  Phone = new FormControl('',/* [Validators.required]*/);
+  Fax = new FormControl('',/* [Validators.required]*/);
+  EffDate = new FormControl('',/* [Validators.required]*/);
+  TermDate = new FormControl('',/* [Validators.required]*/);
+  BillAddressType = new FormControl('',/* [Validators.required]*/);
+  BillAddressTypeDesc = new FormControl('',/* [Validators.required]*/);
+  BillAddr = new FormControl('',/* [Validators.required]*/);
+  Billaddr2 = new FormControl('',/* [Validators.required]*/);
+  BillCity = new FormControl('',/* [Validators.required]*/);
+  Billstate = new FormControl('',/* [Validators.required]*/);
+  Billzip = new FormControl('',/* [Validators.required]*/);
+  Billphone = new FormControl('',/* [Validators.required]*/);
+  Billfax = new FormControl('',/* [Validators.required]*/);
+  BillEffdate = new FormControl('',/* [Validators.required]*/);
+  BillTermdate = new FormControl('',/* [Validators.required]*/);
+  TaxID = new FormControl('',/* [Validators.required]*/);
+  QNXTID = new FormControl('',/* [Validators.required]*/);
+  Source = new FormControl('',/* [Validators.required]*/);
+  echoid = new FormControl('',/* [Validators.required]*/);
+  status = new FormControl('',/* [Validators.required]*/);
+  EchoIPAStatusCode = new FormControl('',/* [Validators.required]*/);
+  EchoIPAStatusDescr = new FormControl('',/* [Validators.required]*/);
+  EchoHospStatusCode = new FormControl('',/* [Validators.required]*/);
+  EchoHOSPStatusDescr = new FormControl('',/* [Validators.required]*/);
+  Echo_ACO_Active = new FormControl('',/* [Validators.required]*/);
+  EchoIPAStatus = new FormControl('',/* [Validators.required]*/);
+  EchoHOSPStatus = new FormControl('',/* [Validators.required]*/);
+  Level1 = new FormControl('',/* [Validators.required]*/);
+  Level2 = new FormControl('',/* [Validators.required]*/);
+  Level3 = new FormControl('',/* [Validators.required]*/);
+  Email = new FormControl('',/* [Validators.required]*/);
+  TaxonomyCode = new FormControl('',/* [Validators.required]*/);
+  spectype = new FormControl('',/* [Validators.required]*/);
+  OrgName= new FormControl('',/* [Validators.required]*/);
 
 
-  LastNameSource = new FormControl('LexisNexis', [Validators.required]);
-  MiddleNameSource = new FormControl('LexisNexis', Validators.required);
-  FirstNameSource = new FormControl('LexisNexis', Validators.required);
-  NPISource = new FormControl('LexisNexis', Validators.required);
-  DOBSource = new FormControl('LexisNexis', Validators.required);
-  BillingAddressSource = new FormControl('LexisNexis', Validators.required);
-  SpecialtySource = new FormControl('LexisNexis', Validators.required);
-  ZipCodeSource = new FormControl('LexisNexis', [Validators.required]);
+  LastNameSource = new FormControl('LexisNexis',/* [Validators.required]*/);
+  MiddleNameSource = new FormControl('LexisNexis',/* Validators.required*/);
+  FirstNameSource = new FormControl('LexisNexis',/* Validators.required*/);
+  NPISource = new FormControl('LexisNexis', /*Validators.required*/);
+  DOBSource = new FormControl('LexisNexis',/* Validators.required*/);
+  BillingAddressSource = new FormControl('LexisNexis',/* Validators.required*/);
+  SpecialtySource = new FormControl('LexisNexis', /*Validators.required*/);
+  ZipCodeSource = new FormControl('LexisNexis',/*[Validators.required]*/);
 
-  title = 'MDMfront';
 
-  SourceProviderData;
+  //SourceProviderData=[{LastName:'1346300860'},{LastName:'1346300860'}];
+  LexisNexisData;
   opened: boolean = true;
 
 
-  constructor(private route: Router, private Service: GetDataService) {
-    
-  }
+  constructor(private router: Router, private service: GetDataService,private route: ActivatedRoute) {}
 
 
   ngOnInit() {
-    //seems like the in-memory-Db can only create one database so use the interface for now.
     this.dataSource.data = TREE_DATA;
-    this.Service.getLexisNexis().subscribe(
-      res=>{
-        this.SourceProviderData = res;
-      }
-    );
-
-    //for static purpose only, needs improvement when comes to practice.
-    for (let NL of LexisNexis) { this.NLNPI = NL.NPI; this.NLLastName = NL.LastName; this.NLMiddleName = NL.MiddleName; this.NLFirstName = NL.FirstName; this.NLDOB = NL.DOB; this.NLBillingAddress = NL.BillingAddress; this.NLSpecialty = NL.Specialty; this.NLZipCode = NL.ZipCode; }
-
+    this.getLexisNexisData();
+   
   }
 
   
@@ -126,19 +168,18 @@ export class GoldenRecordComponent implements OnInit {
   dataSource = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
 
   hasChild = (_: number, node: ExampleFlatNode) => node.expandable;
-  IsChecked() {
-    for (var i = 0; i < this.SourceProviderData.length; i++) {
-      if (this.BillingAddress.value) {
-        this.BillingAddress.setValue(this.SourceProviderData[i].BillingAddress)
-      }
-    }
-  }
 
+
+  getLexisNexisData(): void {
+    const NPI = this.route.snapshot.paramMap.get('NPI');
+    this.service.searchbyNPI(NPI.toString())
+      .subscribe(RES => this.LexisNexisData=RES);
+  }
   Logout() {
-    this.route.navigateByUrl('/login');
+    this.router.navigateByUrl('/login');
   }
 
-  updateMiddleName(middlename, Source) {
+  /*updateMiddleName(middlename, Source) {
     this.MiddleName.setValue(middlename);
     this.MiddleNameSource.setValue(Source);
   }
@@ -169,7 +210,11 @@ export class GoldenRecordComponent implements OnInit {
   updateLastName(middlename, Source) {
     this.LastName.setValue(middlename);
     this.LastNameSource.setValue(Source);
-  }
+  }*/
+
+  //updateBusinessKey(middlename, Source) {
+  //  this.BusinessKey.setValue('middlename');
+    //this.LastNameSource.setValue(Source);
+//}
+
 }
-
-
--- src/app/home/search/search.component.ts
+++ src/app/home/search/search.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Route, Router } from '@angular/router';
 import { FormControl, Validators, FormsModule } from '@angular/forms';
 import { GetDataService } from 'src/app/@core/service/get-data.service';
-import { Subject} from 'rxjs';
-import {debounceTime, distinctUntilChanged, switchMap} from 'rxjs/operators';
+import { Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
 
 
 @Component({
@@ -15,9 +15,12 @@ export class SearchComponent implements OnInit {
   NPI = new FormControl('');
   LexisNexisData;
   private searchTerms = new Subject<string>();
-  
-  constructor(private route: Router,private service: GetDataService,) { }
-  
+
+  constructor(
+    private route: Router,
+    private service: GetDataService,
+  ) { }
+
   search(term: string): void {
     this.searchTerms.next(term);
   }
@@ -32,8 +35,8 @@ export class SearchComponent implements OnInit {
       // switch to new search observable each time the term changes
       switchMap((term: string) => this.service.searchbyNPI(term)),
     );
-   }
+  }
   Search() {
-    this.route.navigateByUrl('/home/GoldenRecord/'+ this.NPI.value);
+    this.route.navigateByUrl('/home/GoldenRecord/' + this.NPI.value);
   }
 }
